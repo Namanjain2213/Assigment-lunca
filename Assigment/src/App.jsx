3fbd63ae1ace49ebc8ad8aa45b997b8121da42d6
Namanjain2213 <nj626433@gmail.com>
2024-09-04 23:09:20 +0530

    new commit

--- Assigment/src/App.jsx
+++ Assigment/src/App.jsx
@@ -5,7 +5,6 @@ import './index.css'
 const App = () => {
   return (
     <div className="min-h-screen  bg-gray-900 text-white flex flex-col items-center justify-center py-0">
-      {/* <h1 className="text-3xl mb-6 max-w-[780px]">Sample</h1> */}
       <Tabs />
       <div className='custom-div' ></div>
       <Gallery />
